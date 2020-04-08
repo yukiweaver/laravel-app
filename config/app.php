7e9ccbc7595db7ecc40dbeca6455532e76f0fd72
yuki ueno <yuki.weaver1204@gmail.com>
2020-04-08 18:05:39 +0900

    定数クラス 定義

--- config/app.php
+++ config/app.php
@@ -225,6 +225,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        // 定数系
+        'UserConst' =>  App\Consts\UserConst::class,
 
     ],
 
